3da2e4058b10b68ca07fb6221b7ceda9c674ee69
Your Name <you@example.com>
2021-02-05 22:31:22 +0900

    Vuejsの学習の記録として残します

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="countup">+</button>
+    <button @click="countdown">-</button>
+    <P>{{ number1 }}</P>
+    <p>{{ number2 }}</P>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return{
+      message : "ボタンのテスト",
+      number1 : 0,
+      number2 : 12000
+    }
+  },
+  methods: {
+    countup() {
+      this.number1 += 1
+    },
+    countdown() {
+      this.number2 -= 1
+    }
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
